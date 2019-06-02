671ab85e19fe0f30c2bced9afbf775805378e66c
Zuzanna Kuzniar <zzuz1015@gmail.com>
2019-06-02 15:14:36 +0200

    update day 4

--- src/main/java/Day4/Serialization.java
+++ src/main/java/Day4/Serialization.java
@@ -1,16 +1,70 @@
 package Day4;
 
 import java.io.*;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class Serialization {
 
-    public static void serialised(Person person) throws IOException {
-        FileOutputStream fileOutputStream = new FileOutputStream("personSerialization.txt");
+    public static void serialiseToFile(Person person, String fileName) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(person);
         objectOutputStream.flush();
         objectOutputStream.close();
     }
+    public static Person deserialiseFromFile(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Person person =(Person) objectInputStream.readObject();
+        return person;
+    }
+
+    public static byte[] serialiseToString_v1(Person person) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(gzipOutputStream);
+        objectOutputStream.writeObject(person);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static Person deserialiseFromString_v1(byte[] objectFromSerialisation) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(objectFromSerialisation);
+        GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
+        ObjectInputStream objectInputStream = new ObjectInputStream(gzipInputStream);
+        Person person = (Person) objectInputStream.readObject();
+        objectInputStream.close();
+
+        return person;
+    }
+
+    public static String serialiseToString(Person person) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(gzipOutputStream);
+        objectOutputStream.writeObject(person);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+    }
+
+    public static Person deserialiseFromString(String objectFromSerialisation) throws IOException, ClassNotFoundException {
+        byte[] bytes = Base64.getDecoder().decode(objectFromSerialisation);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
+        ObjectInputStream objectInputStream = new ObjectInputStream(gzipInputStream);
+        Person person = (Person) objectInputStream.readObject();
+        objectInputStream.close();
+
+        return person;
+    }
+
+
 }
 
 class Person implements Serializable {
@@ -24,24 +78,39 @@ class Person implements Serializable {
         this.lastname = lastname;
     }
 
-    public String getFirstname() {
+    public String getFirstName() {
         return firstname;
     }
 
-    public String getLastname() {
+    public String getLastName() {
         return lastname;
     }
 
-    public Address getAdress() {
+    public Address getAddress() {
         return adress;
     }
 
-    public void setAdress(Address adress) {
+    public void setAddress(Address adress) {
         this.adress = adress;
+    }
+
+    private void writeObject(ObjectOutputStream objectOutputStream) throws IOException {
+        objectOutputStream.defaultWriteObject();
+        objectOutputStream.writeObject(getAddress().getCity());
+    }
+
+    private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        objectInputStream.defaultReadObject();
+        String  city = (String) objectInputStream.readObject();
+        this.setAddress(new Address(city));
     }
 }
 
 class Address{
     private String city;
     public Address(String city){this.city=city;}
+
+    public String getCity() {
+        return city;
+    }
 }
--- src/test/java/Day4/SerializationTest.java
+++ src/test/java/Day4/SerializationTest.java
@@ -1,27 +1,103 @@
 package Day4;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+class SerializationsTest {
 
-class SerializationTest {
-
-    @DisplayName("Serialised and deserialised object should equal")
+    @DisplayName("Serialised and deserialised object should be equal")
     @Test
-    public void serialisedAndDeserialisedObjectShouldEqual(){
-        // serialised
+    void serialisedAndDeserialisedObjectShouldBeEqual() {
+
         Person person = new Person("Robert", "Kareta");
-        person.setAdress(new Address("Cicha 2"));
+        person.setAddress(new Address("Cicha 2"));
+
+        String fileName = "personSerialisation.txt";
+        // serialise object
         try {
-            Serialization.serialised(person);
+            Serialization.serialiseToFile(person, fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // deserialised
-        //chceck equal
+
+        // deserialise object
+        Person personFromSerialisation = null;
+        try {
+            personFromSerialisation = Serialization.deserialiseFromFile(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // check equal
+        Assertions.assertEquals(person.getFirstName(), personFromSerialisation.getFirstName());
+        Assertions.assertEquals(person.getLastName(), personFromSerialisation.getLastName());
+
     }
 
+    @DisplayName("Serialised and deserialised object should be equal - based on String")
+    @Test
+    void serialisedAndDeserialisedObjectShouldBeEqual_v2() {
+        Person person = new Person("Robert", "Kareta");
+        person.setAddress(new Address("Cicha 2"));
+
+        String fileName = "personSerialisation.txt";
+        // serialise object
+        String objectFromSerialisation = null;
+        try {
+            objectFromSerialisation = Serialization.serialiseToString(person);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // deserialise object
+        Person personFromSerialisation = null;
+        try {
+            personFromSerialisation = Serialization.deserialiseFromString(objectFromSerialisation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // check equal
+        Assertions.assertEquals(person.getFirstName(), personFromSerialisation.getFirstName());
+        Assertions.assertEquals(person.getLastName(), personFromSerialisation.getLastName());
+
+    }
+
+    @DisplayName("Serialised and deserialised object should be equal - based on String")
+    @Test
+    void serialisedAndDeserialisedObjectShouldBeEqual_v1() {
+        Person person = new Person("Robert", "Kareta");
+        person.setAddress(new Address("Cicha 2"));
+
+        String fileName = "personSerialisation.txt";
+        // serialise object
+        byte[] objectFromSerialisation = null;
+        try {
+            objectFromSerialisation = Serialization.serialiseToString_v1(person);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // deserialise object
+        Person personFromSerialisation = null;
+        try {
+            personFromSerialisation = Serialization.deserialiseFromString_v1(objectFromSerialisation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // check equal
+        Assertions.assertEquals(person.getFirstName(), personFromSerialisation.getFirstName());
+        Assertions.assertEquals(person.getLastName(), personFromSerialisation.getLastName());
+
+    }
 }
